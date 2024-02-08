b3f914b83cca3732a7b9b8b3b41c10ec90f5f8e0
Gustavo de Araujo Oliveira <gustavo.gunners@hotmail.com>
2024-02-08 16:55:59 -0300

    Create service extension for dependency injection

---
+++ SitewareStore.Service/Extensions/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SitewareStore.Domain.Services.Product;
+using SitewareStore.Domain.Services.Promotion;
+using SitewareStore.Domain.Services.ShoppingCart;
+using SitewareStore.Service.Contracts.Product;
+using SitewareStore.Service.Contracts.Promotion;
+using SitewareStore.Service.Contracts.ShoppingCart;
+
+namespace SitewareStore.Service.Extensions
+{
+    /// <summary>
+    /// Extensões para a camada de serviço
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IChangeProductStatusService, ChangeProductStatusService>();
+            services.AddScoped<IDeleteProductService, DeleteProductService>();
+            services.AddScoped<IGetProductService, GetProductService>();
+            services.AddScoped<IListActiveProductService, ListActiveProductService>();
+            services.AddScoped<IListProductService, ListProductService>();
+            services.AddScoped<ISaveProductService, SaveProductService>();
+
+            services.AddScoped<IChangePromotionStatusService, ChangePromotionStatusService>();
+            services.AddScoped<IDeletePromotionService, DeletePromotionService>();
+            services.AddScoped<IGetPromotionService, GetPromotionService>();
+            services.AddScoped<IListActivePromotionService, ListActivePromotionService>();
+            services.AddScoped<IListPromotionService, ListPromotionService>();
+            services.AddScoped<ISavePromotionService, SavePromotionService>();
+
+            services.AddScoped<IAddShoppingCartItemService, AddShoppingCartItemService>();
+            services.AddScoped<IChangeShoppingCartStatusService, ChangeShoppingCartStatusService>();
+            services.AddScoped<IDeleteShoppingCartItemService, DeleteShoppingCartItemService>();
+            services.AddScoped<IGetShoppingCartService, GetShoppingCartService>();
+            services.AddScoped<IInitializeShoppingCartService, InitializeShoppingCartService>();
+            services.AddScoped<IListShoppingCartService, ListShoppingCartService>();
+            services.AddScoped<IUpdateShoppingCartItemService, UpdateShoppingCartItemService>();
+        }
+    }
+}
