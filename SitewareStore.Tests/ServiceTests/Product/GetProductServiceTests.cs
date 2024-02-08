067c8fb117f85572f2937a441c7582b2d2ec9c13
Gustavo de Araujo Oliveira <gustavo.gunners@hotmail.com>
2024-02-08 19:03:06 -0300

    Unit tests for GetProductService

---
+++ SitewareStore.Tests/ServiceTests/Product/GetProductServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SitewareStore.Domain.Services.Product;
+using SitewareStore.Tests.Helpers;
+using SitewareStore.Tests.Mocks;
+using SitewareStore.Infra.CrossCutting.Extensions;
+
+namespace SitewareStore.Tests.ServiceTests.Product
+{
+    public class GetProductServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnSuccess()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ProductRepositoryMock.BuildSuccess_For_GetSave());
+            services.AddTransient(mock => RepositoryBaseMock.Build());
+
+            var productService = services.GetService<IGetProductService>();
+
+            var serviceResponse = await productService.Execute(Guid.NewGuid());
+
+            Assert.True(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_InvalidId()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ProductRepositoryMock.BuildSuccess_For_GetSave());
+            services.AddTransient(mock => RepositoryBaseMock.Build());
+
+            var productService = services.GetService<IGetProductService>();
+
+            var serviceResponse = await productService.Execute(Guid.Empty);
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_ProductNotFound()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ProductRepositoryMock.BuildFailure_For_Get());
+            services.AddTransient(mock => RepositoryBaseMock.Build());
+
+            var productService = services.GetService<IGetProductService>();
+
+            var serviceResponse = await productService.Execute(Guid.NewGuid());
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_Exception()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ProductRepositoryMock.BuildException_For_Get());
+            services.AddTransient(mock => RepositoryBaseMock.Build());
+
+            var productService = services.GetService<IGetProductService>();
+
+            var serviceResponse = await productService.Execute(Guid.NewGuid());
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+    }
+}
