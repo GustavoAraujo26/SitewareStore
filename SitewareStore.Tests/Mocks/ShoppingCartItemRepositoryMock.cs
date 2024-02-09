345a13aafe2099757a85f5dac97af08b50f21354
Gustavo de Araujo Oliveira <gustavo.gunners@hotmail.com>
2024-02-08 21:14:27 -0300

    Unit tests for DeleteShoppingCartItemService

---
+++ SitewareStore.Tests/Mocks/ShoppingCartItemRepositoryMock.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using Moq;
+using SitewareStore.Domain.Entities;
+using SitewareStore.Domain.Repositories;
+using SitewareStore.Tests.FakeData;
+
+namespace SitewareStore.Tests.Mocks
+{
+    internal static class ShoppingCartItemRepositoryMock
+    {
+        public static Mock<IShoppingCartItemRepository> BuildSuccess()
+        {
+            var mock = new Mock<IShoppingCartItemRepository>();
+
+            mock.Setup(x => x.Get(It.IsAny<SqlConnection>(), It.IsAny<Guid>()))
+                .ReturnsAsync(ShoppingCartItemFakeData.BuildCartItem());
+
+            return mock;
+        }
+
+        public static Mock<IShoppingCartItemRepository> BuildFailure_For_Get()
+        {
+            var mock = new Mock<IShoppingCartItemRepository>();
+
+            mock.Setup(x => x.Get(It.IsAny<SqlConnection>(), It.IsAny<Guid>()))
+                .ReturnsAsync((ShoppingCartItem)null);
+
+            return mock;
+        }
+
+        public static Mock<IShoppingCartItemRepository> BuildFailure_For_Exception()
+        {
+            var mock = new Mock<IShoppingCartItemRepository>();
+
+            mock.Setup(x => x.Get(It.IsAny<SqlConnection>(), It.IsAny<Guid>()))
+                .ThrowsAsync(new Exception("Fatal Error"));
+
+            return mock;
+        }
+    }
+}
---
+++ SitewareStore.Tests/ServiceTests/ShoppingCart/DeleteShoppingCartItemServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SitewareStore.Tests.Mocks;
+using SitewareStore.Infra.CrossCutting.Extensions;
+using SitewareStore.Tests.Helpers;
+using SitewareStore.Domain.Requests;
+using SitewareStore.Tests.Mock;
+using SitewareStore.Domain.Services.ShoppingCart;
+
+namespace SitewareStore.Tests.ServiceTests.ShoppingCart
+{
+    public class DeleteShoppingCartItemServiceTests
+    {
+        [Fact]
+        public async Task ShouldReturnSuccess()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ShoppingCartRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => ShoppingCartItemRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => RepositoryBaseMock.Build().Object);
+
+            var productService = services.GetService<IDeleteShoppingCartItemService>();
+
+            var request = new DeleteShoppingCartItemRequest(Guid.NewGuid(), Guid.NewGuid());
+
+            var serviceResponse = await productService.Execute(request);
+
+            Assert.True(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_InvalidRequest()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ShoppingCartRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => ShoppingCartItemRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => RepositoryBaseMock.Build().Object);
+
+            var productService = services.GetService<IDeleteShoppingCartItemService>();
+
+            var request = new DeleteShoppingCartItemRequest();
+
+            var serviceResponse = await productService.Execute(request);
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_CartNotFound()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ShoppingCartRepositoryMock.BuildFailure_For_Get().Object);
+            services.AddTransient(mock => ShoppingCartItemRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => RepositoryBaseMock.Build().Object);
+
+            var productService = services.GetService<IDeleteShoppingCartItemService>();
+
+            var request = new DeleteShoppingCartItemRequest(Guid.NewGuid(), Guid.NewGuid());
+
+            var serviceResponse = await productService.Execute(request);
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_CartItemNotFound()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ShoppingCartRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => ShoppingCartItemRepositoryMock.BuildFailure_For_Get().Object);
+            services.AddTransient(mock => RepositoryBaseMock.Build().Object);
+
+            var productService = services.GetService<IDeleteShoppingCartItemService>();
+
+            var request = new DeleteShoppingCartItemRequest(Guid.NewGuid(), Guid.NewGuid());
+
+            var serviceResponse = await productService.Execute(request);
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+
+        [Fact]
+        public async Task ShouldReturnFailure_For_Exception()
+        {
+            var services = ServiceCollectionBuilder.BuildServiceCollection();
+
+            services.AddTransient(mock => ShoppingCartRepositoryMock.BuildException_For_Get().Object);
+            services.AddTransient(mock => ShoppingCartItemRepositoryMock.BuildSuccess().Object);
+            services.AddTransient(mock => RepositoryBaseMock.Build().Object);
+
+            var productService = services.GetService<IDeleteShoppingCartItemService>();
+
+            var request = new DeleteShoppingCartItemRequest(Guid.NewGuid(), Guid.NewGuid());
+
+            var serviceResponse = await productService.Execute(request);
+
+            Assert.False(serviceResponse.IsSuccess());
+        }
+    }
+}
